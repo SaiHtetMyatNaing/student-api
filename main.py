0178db503542a4a56d3194b48c7d204e813004f8
Sai Htet Myat Naing <saihtetmyatnaing4@gmail.com>
2025-10-23 11:10:17 +0700

    feat: implement student router with CRUD operations

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 
-
+from app.routers import student
 # Create database tables
 Base.metadata.create_all(bind=engine)
 
@@ -9,6 +9,8 @@ app = FastAPI(
     title="Student Management API",
     version="1.0.0"
 )
+
+app.include_router(student.router)
 
 @app.get("/")
 def root():
---
+++ routers/student.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter , Depends , HTTPException 
+from sqlalchemy.orm import Session
+
+from typing import List
+
+from app.models.student import Student
+from app.database import get_db
+from app.schemas.student import StudentCreate, StudentResponse , StudentUpdate
+
+
+router = APIRouter(prefix="/students")
+
+# Create - Add new student
+@router.post("/" , response_model=StudentResponse , status_code=201)
+def create_student(student : StudentCreate , db: Session = Depends(get_db)):
+    
+    # check if the student exist 
+    existing_student = db.query(Student).filter(Student.email == student.email).first()
+    if existing_student:
+        raise HTTPException(status_code=400 , detail="Email already exists!")
+    
+    
+    # if the student doesn't exit create new one
+    
+    new_student = Student(**student.model_dump())
+
+    db.add(new_student)
+    db.commit()
+    db.refresh(new_student)
+    
+    return new_student
+
+# Get - Get all student
+@router.get("/" , response_model=List[StudentResponse] , status_code=200)
+
+def get_all_students( skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    
+    students = db.query(Student).offset(skip).limit(limit).all()
+    
+    return students
+
+# READ - Get single student
+@router.get("/{student_id}", response_model=StudentResponse , status_code=200)
+
+def get_student(student_id: int, db: Session = Depends(get_db)):
+    
+    student = db.query(Student).filter(Student.id == student_id).first()
+    
+    if not student:
+        
+        raise HTTPException(status_code=404, detail="Student not found")
+    
+    return student
